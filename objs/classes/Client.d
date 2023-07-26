691842c3d4cba78cdb73c075ed599d53538a1cfc
Aleksei Nedovesov <xxxleshaxxx4@gmail.com>
2023-07-26 14:59:20 +0200

    deleted files .0 &.d

--- objs/classes/Client.d
+++
@@ -1,8 +0,0 @@
-objs/classes/Client.o: srcs/classes/Client.cpp includes/Client.hpp \
-  includes/stdafx.hpp includes/CmdList.hpp
-
-includes/Client.hpp:
-
-includes/stdafx.hpp:
-
-includes/CmdList.hpp:
