c901b13253eda84773137be66f6521d9a8d54e19
Aleksei Nedovesov <xxxleshaxxx4@gmail.com>
2023-07-26 18:01:57 +0200

    correct KICK

--- srcs/execute/commands/KICK.cpp
+++ srcs/execute/commands/KICK.cpp
@@ -4,14 +4,14 @@
 //Команда для удаления юзеров из каналов.
 void KICK(Server *server, Client *client, cmdList *cmd)
 {
-	std::string channel_name = cmd->parameters[0].substr(1, cmd->parameters[0].size() - 1);
 	std::string nickname = client->GetNickname();
-
-	if (channel_name.empty() || cmd->parameters.size() != 2)
+	std::string reason = "Sorry";
+	if (cmd->parameters.size() < 2)
 	{
-		client->_send_buff.append(ERR_NEED_MORE_PARAMS(nickname, channel_name));
+		client->_send_buff.append(ERR_NEED_MORE_PARAMS(nickname, cmd->command));
 		return;
 	}
+	std::string channel_name = cmd->parameters[0].substr(1, cmd->parameters[0].size() - 1);
 	std::map<std::string, Channel*>::iterator it = server->GetChannels()->find(channel_name);
 
 	if (it == server->GetChannels()->end())
@@ -31,7 +31,7 @@ void KICK(Server *server, Client *client, cmdList *cmd)
 	}
 
 	std::map <std::string, Client*>::iterator it_user = chennel->GetClientList().find(cmd->parameters[1]);
-	
+	std::string user_name = cmd->parameters[1];
 	if(it_user == chennel->GetClientList().end())
 	{
 		client->_send_buff.append(ERR_USER_NO_TIN_CHANNEL(nickname, channel_name));
@@ -44,6 +44,18 @@ void KICK(Server *server, Client *client, cmdList *cmd)
 		return;
 	}
 
-	chennel->RemoveClient(cmd->parameters[1]);
+	if(!cmd->trailing.empty())
+		reason = cmd->trailing;
+
+	std::map<std::string, Client*> clients_on_chennel = chennel->GetClientList();
+    std::map <std::string, Client*>::iterator it_client_it = clients_on_chennel.begin();
+	std::cout << "g" << std::endl;
+	for(; it_client_it != clients_on_chennel.end(); it_client_it++)
+	{
+		std::cout << "sdds" << std::endl;
+		it_client_it->second->_send_buff.append(RPL_KICK(nickname, channel_name, user_name, reason));
+	}
+	chennel->RemoveClient(user_name);
+
 	//TODO проверить отправляют ли сообщения
 }
